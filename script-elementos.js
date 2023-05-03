b2b2df4c024b980f191fd1d06b5c546a9d639426
Judson Paiva <judson.paiva@hotmail.com>
2023-05-03 23:06:29 +0100

    funcoes default do jquery

--- script-elementos.js
+++ script-elementos.js
@@ -7,3 +7,19 @@ function exibeElementoPorTag(tag){
 function ocultaElementoPorTag(tag){
     $(tag).hide();
 }
+
+function ocultarPorClasse(elemento){
+    $('.'+elemento).hide();
+}
+
+function mostrarPorClasse(elemento){
+    $('.'+elemento).show();
+}
+
+function ocultarPorID(elemento){
+    $('#'+elemento).hide();
+}
+
+function mostrarPorID(elemento){
+    $('#'+elemento).show();
+}
